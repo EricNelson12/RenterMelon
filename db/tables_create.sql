ae4cdb67f8a588776306bb6a76980edd4eca0642
Brittany Miller <Brittany Miller>
2017-02-27 17:07:47 -0800

    Made table name captitalization consistent

--- db/tables_create.sql
+++ db/tables_create.sql
@@ -1,34 +1,38 @@
 /* 
-Rental(rID, title, price, desc, area, address, link, addDate)
 
-Contact(rID, name, phone, email)
-FK rID ref Rental(rID)
+SCHEMA:
 
-Users(username, pword, email, name, admin)
+	Rental(rID, title, price, desc, area, address, link, addDate)
 
-Report(desc, type, time, rID, username)
-FK rID ref Rental(rID), FK username ref Users(username)
+	Contact(rID, name, phone, email)
+	FK rID ref Rental(rID)
 
-SavedAds(rID, username)
-FK rID ref Rental(rID), FK username ref Users(username)
+	Users(username, pword, email, name, admin)
+
+	Report(desc, type, time, rID, username)
+	FK rID ref Rental(rID), FK username ref Users(username)
+
+	SavedAds(rID, username)
+	FK rID ref Rental(rID), FK username ref Users(username)
 
 
-This isn't finished yet yall
+
 
 */ 
 
 
 
-CREATE TABLE Rental (
+
+CREATE TABLE rental (
 
 	rID INTEGER AUTO_INCREMENT, 
-	title VARCHAR(50),
-	price DECIMAL(7,2),
+	title VARCHAR(50) NOT NULL,
+	price DECIMAL(7,2) NOT NULL,
 	description VARCHAR(400),
 	area VARCHAR(20),
 	address VARCHAR(50),
 	link VARCHAR(200),
-	datedAdded DATETIME DEFAULT CURRENT_TIMESTAMP, --I couldn't get this to run with just the date. 
+	datedAdded DATETIME DEFAULT CURRENT_TIMESTAMP, 
 	PRIMARY KEY (rID)
 
 
@@ -43,14 +47,14 @@ CREATE TABLE contact (
 	phone VARCHAR(15),
 	email VARCHAR(30),
 	PRIMARY KEY (rID, name),
-	FOREIGN KEY (rID) REFERENCES Rental(rID)
+	FOREIGN KEY (rID) REFERENCES rental(rID)
 		ON DELETE CASCADE 
 		ON UPDATE CASCADE 
 
 );
 
 
-CREATE TABLE Users (
+CREATE TABLE users (
 
 	username VARCHAR(20),
 	password VARCHAR(20) NOT NULL,
@@ -60,4 +64,39 @@ CREATE TABLE Users (
 
 );
 
+/*
+		Below here not used in release 1 but they seemed easy enough to have in here. 
+*/
 
+
+CREATE TABLE savedAds ( 
+
+	username VARCHAR(20),
+	rID INTEGER,
+	PRIMARY KEY (username,rID),
+	FOREIGN KEY (rID) REFERENCES Rental(rID)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE, 
+	FOREIGN KEY (username) REFERENCES Users(username)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE 
+
+
+);
+
+CREATE TABLE report ( 
+	
+	username VARCHAR(20),
+	rID INTEGER,
+	timeAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
+	reportType VARCHAR(25),
+	description VARCHAR(100),
+	PRIMARY KEY (username, timeAdded),
+	FOREIGN KEY (rID) REFERENCES Rental(rID)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE, 
+	FOREIGN KEY (username) REFERENCES Users(username)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE 
+
+);
